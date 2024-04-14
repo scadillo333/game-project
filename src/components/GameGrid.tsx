227b96d8f4c6617a168fbfecdf0d681df396e858
Alfredo Cadillo <sebastian.cadillo3@gmail.com>
2024-04-14 09:36:30 -0500

    Refactor: Romve duplicated styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,6 +4,7 @@ import { Divider, SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
   const Skeletons = [1, 2, 3, 4, 5, 6];
@@ -12,9 +13,15 @@ const GameGrid = () => {
       {error && <Text className="text-danger">{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
         {isLoading &&
-          Skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+          Skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
